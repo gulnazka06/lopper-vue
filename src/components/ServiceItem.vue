4033ebfb27b0307d34cfe535378d514b211c509c
Gulnazka <khamatnurova1997@mail.ru>
2019-09-30 15:17:26 +0300

    Add services layout

---
+++ src/components/ServiceItem.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="chopping">
+    <img src="../assets/img/chopping-icon.svg" alt="">
+    <h5>Chopping heads</h5>
+    <p>Tincidunt lobortis feugiat vivamus at
+        <br>augue eget. Risus feugiat in ante metus
+        <br>dictum. Amet aliquam id diam maecenas.</p>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "ServiceItem"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .chopping {
+
+        img {
+            border-radius: 50%;
+            background-color: var(--chopping-bg-color);
+            border: 1px solid var(--chopping-border-color);
+            padding: 25px 22px 26px 24px;
+        }
+
+        h5 {
+            font-weight: 500;
+            font-size: 18px;
+            line-height: 27px;
+            color: var(--green);
+            margin: 18px 0 26px;
+        }
+
+        p {
+            font-size: 16px;
+            line-height: 26px;
+            color: var(--service-color);
+        }
+    }
+</style>
---
+++ src/components/Services.vue
@@ -0,0 +1,80 @@
+<template>
+<div class="service">
+    <div class="container">
+        <img src="../assets/img/map.png" alt="">
+        <h4>WE ARE AMAZING</h4>
+        <h3>Our Services</h3>
+        <p>Quam adipiscing vitae proin sagittis nisl rhoncus mattis
+            <br>rhoncus urna. Quam elementum pulvinar etiam non. Eu
+            <br>consequat.</p>
+        <div class="list">
+            <ServiceItem></ServiceItem>
+            <ServiceItem></ServiceItem>
+            <ServiceItem></ServiceItem>
+            <ServiceItem></ServiceItem>
+            <ServiceItem></ServiceItem>
+            <ServiceItem></ServiceItem>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+    import ServiceItem from "@/components/ServiceItem";
+    export default {
+        name: "Services",
+        components: {ServiceItem}
+    }
+</script>
+
+<style lang="scss" scoped>
+    .service {
+        background-image: url("../assets/img/service-bg.png");
+        position: relative;
+        padding-bottom: 98px;
+
+        .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+
+            img {
+                position: absolute;
+                top: 122px;
+            }
+
+
+            h4 {
+                font-weight: 800;
+                font-size: 12px;
+                line-height: 26px;
+                letter-spacing: 6px;
+                color: var(--rose);
+                margin: 262px 0 12px;
+            }
+
+            h3 {
+                font-weight: 500;
+                font-size: 45px;
+                line-height: 67px;
+                color: var(--service-color);
+                text-align: center;
+            }
+
+            p {
+                font-weight: 800;
+                font-size: 18px;
+                line-height: 29px;
+                text-align: center;
+                color: var(--service-color);
+                margin: 23px 0 51px;
+            }
+
+            .list {
+                display: grid;
+                grid-template-columns: 1fr 1fr 1fr;
+                grid-gap: 70px;
+            }
+        }
+    }
+</style>
